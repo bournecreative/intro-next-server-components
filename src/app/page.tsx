7885ef6df862b2080a6f03617a2f80199481884d
Christian <bournecreative@gmail.com>
2024-06-22 02:33:03 -0500

    create server component to fetch the list of snippets in our database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
+import { db } from "@/db";
 
-export default function Home() {
+export default async function Home() {
+  const snippets = await db.snippet.findMany()
+  
+  const renderedSnippets = snippets.map((snippet) => {
+    return (
+      <div key={snippet.id}>
+        {snippet.title}
+        {snippet.code}
+      </div>
+    )
+  })
   return (
-   <main>
-    Start
-   </main>
+    <div>
+      {renderedSnippets}
+    </div>
   );
 }
