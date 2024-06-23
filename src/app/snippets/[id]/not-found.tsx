e5fc60186b513ed41f2c296a746a564292b266d8
Christian <bournecreative@gmail.com>
2024-06-22 22:36:48 -0500

    create a not-found page specific for the snippets pages

---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+export default function SnippetNotFound() {
+    return (
+        <div>
+            <h1 className="text-xl bold">
+                Sorry, but we that snippet could not be found. 
+            </h1>
+        </div>
+    )
+}
